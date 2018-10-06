5acb358c1f9f8e9c34cd2509b5f026d5d73213d5
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-10-05 23:42:51 -0500

    Style changes

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Button, Icon } from '../../node_modules/antd';
+import { Button, Icon } from 'antd';
 
 export class Music extends Component {
   constructor(props) {
@@ -29,7 +29,7 @@ export class Music extends Component {
 
   getZones = async () => {
     try {
-      const req = await axios.get('http://localhost:5005/zones');
+      const req = await axios.get('http://192.168.1.202:5005/zones');
       if (req.status === 200) {
         this.setState(state => ({
           zones: req.data,
@@ -51,29 +51,26 @@ export class Music extends Component {
 
   togglePlayState = async () => {
     try {
-      await axios.get(`http://localhost:5005/${this.state.coordinator}/playpause`);
+      await axios.get(`http://192.168.1.202:5005/${this.state.coordinator}/playpause`);
     } catch (e) {}
   };
 
   prevTrack = async () => {
     try {
-      await axios.get(
-        `http://localhost:5005/${this.state.coordinator}/previous`
-      );
-
+      await axios.get(`http://192.168.1.202:5005/${this.state.coordinator}/previous`);
     } catch (e) {}
   };
 
   nextTrack = async () => {
     try {
-      await axios.get(`http://localhost:5005/${this.state.coordinator}/next`);
+      await axios.get(`http://192.168.1.202:5005/${this.state.coordinator}/next`);
     } catch (e) {}
   };
 
   incrementDecrementVolume = async amount => {
     try {
       await axios.get(
-        `http://localhost:5005/${this.state.coordinator}/groupVolume/${amount}`
+        `http://192.168.1.202:5005/${this.state.coordinator}/groupVolume/${amount}`
       );
     } catch (e) {}
   };
@@ -81,10 +78,11 @@ export class Music extends Component {
   render() {
     return (
       <div>
-        <br />
-        <div className="now-playing">
-          <div className="room">Playing in {this.state.coordinator}</div>
-          <div className="album-art">
+        <div className="card">
+          <div className="meta" style={{ letterSpacing: '2px' }}>
+            Playing in <span style={{ fontWeight: '500' }}>{this.state.coordinator}</span>
+          </div>
+          <div className="image">
             <img
               src={this.state.currentTrack.absoluteAlbumArtUri}
               width="150"
@@ -92,31 +90,27 @@ export class Music extends Component {
               alt={this.state.currentTrack.album}
             />
           </div>
-          <div className="title">{this.state.currentTrack.title}</div>
-          <div className="artist">{this.state.currentTrack.artist}</div>
+          <h1>{this.state.currentTrack.title}</h1>
+          <h2>{this.state.currentTrack.artist}</h2>
+          <div className="actions">
+            <div className="button" onClick={() => this.incrementDecrementVolume('-3')}>
+              <Icon type="minus" />
+            </div>
+            <div className="button" onClick={this.prevTrack}>
+              <Icon type="step-backward" />
+            </div>
+            <div className="button" onClick={this.togglePlayState}>
+              <Icon type="caret-right" theme="outlined" />
+              <Icon type="pause" theme="outlined" />
+            </div>
+            <div className="button" onClick={this.nextTrack}>
+              <Icon type="step-forward" />
+            </div>
+            <div className="button" onClick={() => this.incrementDecrementVolume('+3')}>
+              <Icon type="plus" />
+            </div>
+          </div>
         </div>
-        <Button
-          size="large"
-          shape="circle"
-          onClick={() => this.incrementDecrementVolume('-3')}
-          icon="minus"
-        />
-        &nbsp;
-        <Button.Group>
-          <Button size="large" onClick={this.prevTrack} icon="step-backward" />
-          <Button size="large" onClick={this.togglePlayState}>
-            <Icon type="caret-right" theme="outlined" />
-            <Icon type="pause" theme="outlined" />
-          </Button>
-          <Button size="large" onClick={this.nextTrack} icon="step-forward" />
-        </Button.Group>
-        &nbsp;
-        <Button
-          size="large"
-          shape="circle"
-          onClick={() => this.incrementDecrementVolume('+3')}
-          icon="plus"
-        />
       </div>
     );
   }
